f24744bc4dcbeac9652b3b7aca969a04ea1f318a
Adam Bougrine <99375543+DarkFlyper@users.noreply.github.com>
2024-03-11 15:55:16 -0400

    Update LiveGame.swift

--- Sources/ValorantAPI/Requests/LiveGame.swift
+++ Sources/ValorantAPI/Requests/LiveGame.swift
@@ -97,7 +97,7 @@ private struct PickAgentRequest: GetJSONRequest, LiveGameRequest {
     typealias Response = LivePregameInfo
 }
 
-private struct DodgeAgentRequest: GetJSONRequest, LiveGameRequest {
+private struct DodgeAgentRequest<Response: Decodable>: GetJSONRequest, LiveGameRequest {
     var httpMethod: String { "POST" }
 
     var matchID: Match.ID
