126b2fcdc349f6bc1fe7d225c99af1062b5768b1
Adam Bougrine <99375543+DarkFlyper@users.noreply.github.com>
2024-03-12 15:40:17 -0400

    Update LiveGame.swift

--- Sources/ValorantAPI/Requests/LiveGame.swift
+++ Sources/ValorantAPI/Requests/LiveGame.swift
@@ -50,7 +50,7 @@ extension ValorantClient {
     }
 
     /// Dodges the agent selection process.
-    public func dodgeAgent(in matchID: Match.ID) async throws [Match.ID] {
+    public func dodgeAgent(in matchID: Match.ID) async throws -> Match.ID {
         try await send(DodgeAgentRequest(matchID: matchID))
     }
 }
